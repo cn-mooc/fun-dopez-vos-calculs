9402edce191c0cf765dfdfe7ae4a02805bda6750
richard <randria@users.noreply.github.com>
2022-10-14 18:37:00 +0200

    Update install-valgrind.sh

--- v2/module_1/install-valgrind.sh
+++ v2/module_1/install-valgrind.sh
@@ -3,10 +3,24 @@
 # Version 1.0 @ Thu Oct 13 18:19:11 CEST 2022
 #
 set -x
+
+FILE_VER=3.19.0
+FILE_SRC=valgrind-${FILE_VER}.tar.bz2
+FILE_URL="https://sourceware.org/pub/valgrind/${FILE_SRC}"
+WORK_DIR=`mktemp -d`
+
+# --- update apt db
 apt-get --purge valgrind
-hash git || exit 1
-git clone https://sourceware.org/git/valgrind.git && \
-cd valgrind && \
-./autogen.sh && \
-./configure && \
-make
+
+# --- download source
+cd $WORK_DIR && {
+    wget -q -c $FILE_URL && \ 
+    bzip2 -d -c ${FILE_SRC} | tar xf -
+    cd valgrind-${FILE_VER} && {
+        ./autogen.sh && \
+        ./configure --prefix=/usr/local/bin && \
+        make && make install
+    }
+}
+
+rm -rf $WORK_DIR 2>/dev/null
