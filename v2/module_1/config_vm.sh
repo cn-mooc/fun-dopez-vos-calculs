e3288b74d1ec40b6440a563d8a287d234f7075e7
Richard Randriatoamanana <richard.randriatoamanana@ls2n.fr>
2022-10-14 15:39:45 +0200

    added main and funcs

--- v2/module_1/config_vm.sh
+++ v2/module_1/config_vm.sh
@@ -21,38 +21,48 @@ fatal()
 export FUN_GITDIR=fun-dopez-vos-calculs.git
 
 # === APT Stuff ===
-export DEBIAN_FRONTEND=noninteractive
-APT_OPT=""
-DIST_CODENAME=$(lsb_release -cs)
-if [ $(lsb_release -is) = "Debian" ]; then
-    apt-get install -y software-properties-common dirmngr
-        if [[ $DIST_CODENAME == "stretch" || $DIST_CODENAME == "buster" ]]; then
-        apt-add-repository "deb http://deb.debian.org/debian ${DIST_CODENAME}-backports main";
+apt_stuff(){
+    export DEBIAN_FRONTEND=noninteractive
+    APT_OPT=""
+    DIST_CODENAME=$(lsb_release -cs)
+    if [ $(lsb_release -is) = "Debian" ]; then
+        apt-get install -y software-properties-common dirmngr
+            if [[ $DIST_CODENAME == "stretch" || $DIST_CODENAME == "buster" ]]; then
+            apt-add-repository "deb http://deb.debian.org/debian ${DIST_CODENAME}-backports main";
+        fi
+        APT_OPT="-t ${DIST_CODENAME}-backports --allow-unauthenticated"
+        apt-get -y --allow-unauthenticated dist-upgrade
+    elif [ $DIST_CODENAME != "focal" ]; then
+        apt-add-repository -y ppa:ansible/ansible
     fi
-    APT_OPT="-t ${DIST_CODENAME}-backports --allow-unauthenticated"
-    apt-get -y --allow-unauthenticated dist-upgrade
-elif [ $DIST_CODENAME != "focal" ]; then
-    apt-add-repository -y ppa:ansible/ansible
-fi
-apt-get update
-apt-get install $APT_OPT -y ansible
+    apt-get update
+    apt-get install $APT_OPT -y ansible
+}
 
 # === ANSIBLE Stuff ===
 # --- if we are inside the git repo directory
-if [ -f ansible.yml ]; then
-    git pull
-else
-    if [ -d "$FUN_GITDIR" ]; then
-        fatal "Please remove current [$FUN_GITDIR] first..."
-        return 1
-    fi
-    git clone https://github.com/cn-mooc/$FUN_GITDIR && \
-    cd fun-dopez-vos-calculs/v2/module_1 && {
-        if [ -f ansible.yml ]; then
-            # --- Remove the comment below if you want to run debug mode
-            #export ANSIBLE_DEBUG=true
-            export ANSIBLE_VERBOSITY=3
-            ansible-playbook -c local -i 127.0.0.1, -b -e 'ansible_python_interpreter=/usr/bin/python3' ansible.yml
+ansible_stuff(){
+    if [ -f ansible.yml ]; then
+        git pull
+    else
+        if [ -d "$FUN_GITDIR" ]; then
+            fatal "Please remove current [$FUN_GITDIR] first..."
+            return 1
         fi
-    }
-fi
+        git clone https://github.com/cn-mooc/$FUN_GITDIR && \
+        cd fun-dopez-vos-calculs/v2/module_1 && {
+            if [ -f ansible.yml ]; then
+                # --- Remove the comment below if you want to run debug mode
+                #export ANSIBLE_DEBUG=true
+                export ANSIBLE_VERBOSITY=3
+                ansible-playbook -c local -i 127.0.0.1, -b -e 'ansible_python_interpreter=/usr/bin/python3' ansible.yml
+            fi
+        }
+    fi
+}
+
+# === MAIN ===
+{
+    apt_stuff
+    ansible_stuff
+}
